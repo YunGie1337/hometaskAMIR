fb12c51ed1d327147ce28fa0270855a81dd12871
YunGie1337 <amirnasirov221730@gmail.com>
2022-10-05 23:29:38 +0600

    amir thebest

---
+++ bin/amir_yoo.dart
@@ -0,0 +1,5 @@
+import 'package:amir_yoo/amir_yoo.dart' as amir_yoo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${amir_yoo.calculate()}!');
+}
---
+++ test/amir_yoo_test.dart
@@ -0,0 +1,8 @@
+import 'package:amir_yoo/amir_yoo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
